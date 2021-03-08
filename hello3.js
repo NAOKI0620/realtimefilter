69b38dd536a7238c408d2d43c94ea666180fc39e
EC2 Default User <ec2-user@ip-172-31-10-101.us-east-2.compute.internal>
2021-03-08 04:05:15 +0000

    変更3

--- hello3.js
+++ hello3.js
@@ -15,16 +15,24 @@ let kamaitati=$('.image-element-text4').text();
 
 if (chono.indexOf(inputText) > -1) {
   $('.photot,.photoh,.photok').addClass('hidden');
+  
 }
-
 if(tidori.indexOf(inputText) > -1) {
   $('.photoc,.photoh,.photok').addClass('hidden');
 }
 if(hamabe.indexOf(inputText) > -1){
   $('.photoc,.photot,.photok').addClass('hidden');
-  
 }
+
 if(kamaitati.indexOf(inputText) > -1){
   $('.photoc,.photot,.photoh').addClass('hidden');
+
 }
+
 }
+
+
+
+
+
+
