babb928e0140d8be8537c6610a89ae9b8ed1ac28
EC2 Default User <ec2-user@ip-172-31-10-101.us-east-2.compute.internal>
2021-03-08 02:01:50 +0000

    変更3

--- hello3.js
+++ hello3.js
@@ -8,10 +8,23 @@ let inputText = $(this).val();
 console.log(inputText);
 
 let chono=$('.image-element-text').text();
-console.log(chono);
+let tidori=$('.image-element-text2').text();
+let hamabe=$('.image-element-text3').text();
+let kamaitati=$('.image-element-text4').text();
 
-if (inputText.indexOf(chono) > -1) {
+
+if (chono.indexOf(inputText) > -1) {
   $('.photot,.photoh,.photok').addClass('hidden');
 }
 
+if(tidori.indexOf(inputText) > -1) {
+  $('.photoc,.photoh,.photok').addClass('hidden');
+}
+if(hamabe.indexOf(inputText) > -1){
+  $('.photoc,.photot,.photok').addClass('hidden');
+  
+}
+if(kamaitati.indexOf(inputText) > -1){
+  $('.photoc,.photot,.photoh').addClass('hidden');
+}
 }
